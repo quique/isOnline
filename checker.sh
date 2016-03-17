c975b670c9a5c04060edfd519060143b3e4ac923
Enrique Matías Sánchez <quique@unizar.es>
2016-03-17 16:03:14 +0100

    Add a threshold to avoid false positives
    The first time a website is down, the script just shows a warning.
    If the following time the script is run the site is still down, an error is shown and mail is sent.

--- checker.sh
+++ checker.sh
@@ -2,10 +2,10 @@
 # Website status checker. by ET (etcs.me)
 
 #WORKSPACE=/scripts/isOnline
-WORKSPACE=.
-# list of websites. each website in new line. leave an empty line in the end.
+WORKSPACE=`dirname "$(readlink -f "$0")"`
+# list of websites. each website in new line. Leave an empty line in the end.
 LISTFILE=$WORKSPACE/websites.lst
-# Send mail in case of failure to. leave an empty line in the end.
+# Send mail in case of failure to. Leave an empty line in the end.
 EMAILLISTFILE=$WORKSPACE/emails.lst
 # Temporary dir
 TEMPDIR=$WORKSPACE/cache
@@ -14,7 +14,7 @@ CANARY=Directorio
 
 # `Quiet` is true when in crontab; show output when it's run manually from shell.
 # Set THIS_IS_CRON=1 in the beginning of your crontab -e.
-# else you will get the output to your email every time
+# Otherwise you will get the output to your email every time.
 if [ -n "$THIS_IS_CRON" ]; then QUIET=true; else QUIET=false; fi
 
 function test {
@@ -30,12 +30,17 @@ function test {
         if [ "$QUIET" = false ] ; then
             echo -e "\e[32m[ok]\e[0m"
         fi
-        # remove .temp file if exist 
+        # Remove temporary file if it exists.
         if [ -f $TEMPDIR/$filename ]; then rm -f $TEMPDIR/$filename; fi
     else
         # website down
-        if [ "$QUIET" = false ] ; then echo -e "\e[31m[DOWN]\e[0m"; fi
         if [ ! -f $TEMPDIR/$filename ]; then
+            # Down for the first time.  Just show a warning.
+            if [ "$QUIET" = false ] ; then echo -e "\e[33m[down]\e[0m"; fi
+            echo > $TEMPDIR/$filename
+        else
+            # It keeps being down. Show an error and send mail.
+            if [ "$QUIET" = false ] ; then echo -e "\e[31m[DOWN]\e[0m"; fi
             while read e; do
                 : # Uncomment one of the mail commands to be notified via email
                 # using mailx command
@@ -43,7 +48,6 @@ function test {
                 # using mail command
                 #mail -s "$p WEBSITE DOWN" "$EMAIL"
             done < $EMAILLISTFILE
-            echo > $TEMPDIR/$filename
         fi
     fi
 }
